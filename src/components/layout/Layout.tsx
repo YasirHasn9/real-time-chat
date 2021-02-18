8864850cc8b22de78584a05199d58b8d8e5d7d77
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-17 23:12:53 -0800

    apply global style

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,3 +1,6 @@
+// global styled 
+import {GlobalStyle} from "../../global/styled.global"
+
 // components
 import {SidebarContainer}  from "../sidebar/Sidebar"
 // styled
@@ -7,6 +10,7 @@ import {Container} from "./Layout.styled"
 export function Layout (){
     return (
         <Container>
+            <GlobalStyle />
             <SidebarContainer />
             <div>messages</div>
         </Container>
