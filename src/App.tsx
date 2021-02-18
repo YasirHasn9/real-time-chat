986c77252a883434b495edc29cd2caf044f8fda1
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-17 23:05:03 -0800

    deleted unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Layout } from './components/layout/Layout';
-import "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Layout />
     </div>
   );
